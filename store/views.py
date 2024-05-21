dc9d1ceae8e43804d0e2fefb2c88f81c0de23ae9
Ahmad-Hazrati <hazrati.ahmad@gmail.com>
2024-05-21 10:33:54 +0200

    Create User register form and add registeration functionality

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User 
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms 
 
 
 def home(request):
@@ -23,7 +27,7 @@ def login_user(request):
             messages.success(request, ("You have been logged in successfully"))
             return redirect('home')
         else:
-            messages.error(request, ("Seems there was occurred an error, please try again!"))
+            messages.error(request, ("Whoops! Seems there was occurred an error, please try again."))
             return redirect('login')
     else:
         return render(request, 'login.html', {})
@@ -33,4 +37,23 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You have been logged out successfully."))
     return redirect('home')
-    
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # Login User
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("You have registered Successfully. Welcome!"))
+            return redirect('home')
+        else:
+            messages.error(request, ("Whoops! Seems there was occurred an error, please try again."))
+            return redirect('register')
+    else :
+        return render(request, 'register.html', {'form':form})

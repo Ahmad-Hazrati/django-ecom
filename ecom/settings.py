3322b2b4f51412ae9fc6299fbd0aedb50ae05877
Ahmad-Hazrati <hazrati.ahmad@gmail.com>
2024-05-25 22:03:46 +0200

    Add js code, Cart_Add view and Add function to add products to the cart/sessions

--- ecom/settings.py
+++ ecom/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'cart.context_processor.cart',
+                'cart.context_processors.cart',
             ],
         },
     },

807868da7543c42b0d4ce77020cd131fd674a4b9
Ahmad-Hazrati <hazrati.ahmad@gmail.com>
2024-05-27 09:42:53 +0200

    Update shopping cart summary page

--- cart/cart.py
+++ cart/cart.py
@@ -1,4 +1,4 @@
-
+from store.models import Product
 class Cart():
     def __init__(self, request):
         self.session = request.session 
@@ -28,3 +28,13 @@ class Cart():
         
     def __len__(self):
         return len(self.cart)
+    
+    
+    def get_products(self):
+        # Get ids from cart
+        product_ids = self.cart.keys()
+        
+        # Use ids to lookup products in database model
+        products = Product.objects.filter(id__in=product_ids)
+        
+        return products

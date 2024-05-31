41911929817d39a6b2c38112c5d034aea49a53a8
Ahmad-Hazrati <hazrati.ahmad@gmail.com>
2024-05-31 05:47:52 +0200

    Add cart totals functionality

--- cart/cart.py
+++ cart/cart.py
@@ -47,6 +47,29 @@ class Cart():
         return quantities
     
     
+    def cart_total(self):
+        # Get product ids
+        product_ids = self.cart.keys()
+        # Lookup those keys in our products database
+        products = Product.objects.filter(id__in=product_ids)
+        # Get quantities
+        quantities = self.cart
+        # Start counting at 0
+        total = 0
+        for key, value in quantities.items():
+            # Convert key string into integer so we can add do the Math
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    if product.is_sale:
+                        total = total + (product.sale_price * value)
+                    else:
+                        total = total + (product.price * value)
+        return total
+        
+        
+    
+    
     def update(self, product, quantity):
         product_id = str(product)
         product_qty = int(quantity)
